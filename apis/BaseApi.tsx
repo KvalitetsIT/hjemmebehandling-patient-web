e5caa8b2764f512d507d62d20a22437e3ffa1d5b
Mikkel Birch <mib@kvalitetsit.dk>
2021-12-09 15:50:06 +0100

    baseapi errorhandling

--- apis/BaseApi.tsx
+++ apis/BaseApi.tsx
@@ -1,20 +1,33 @@
-import {BaseApiError} from "./../apis/Errors/BaseApiError"
+import { BaseApiError } from "./../apis/Errors/BaseApiError"
 
 export default class BaseApi {
-    
-    async HandleError(error : any) : Promise<any> {
+
+    async HandleError(error: any): Promise<any> {
         console.debug("Transforming error to ServiceError")
         console.log(error)
-        if(error instanceof Response){
+        if (error instanceof Response) {
             let response = error as Response
             let bodyText = await error.text()
-            throw new BaseApiError(response, bodyText,response.status!)
+            try {
+                let json: bodyObj = JSON.parse(bodyText)
+                throw new BaseApiError(response, json.error, response.status!)
+            } catch(error){
+                console.log(error)
+                throw new BaseApiError(response, bodyText, response.status!)
+            }
+            
         }
-        
+
         throw error;
-        
+
     }
 
 
 }
-  
+
+interface bodyObj {
+    timestamp?: Date;
+    status?: number;
+    error?: string;
+    path?: string
+}
