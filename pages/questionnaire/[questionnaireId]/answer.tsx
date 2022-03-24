64e76a53506811b4565480c487156df2fbc2d6ed
Mikkel Birch <mib@kvalitetsit.dk>
2022-03-24 07:58:05 +0100

    Change color of toasts

--- pages/questionnaire/[questionnaireId]/answer.tsx
+++ pages/questionnaire/[questionnaireId]/answer.tsx
@@ -22,7 +22,7 @@ import { CallToActionMessage } from "@kvalitetsit/hjemmebehandling/Models/CallTo
 import { CallToActionError } from "../../../components/Errors/CallToActionError";
 import { DialogError } from "@kvalitetsit/hjemmebehandling/Errorhandling/DialogError";
 import ErrorIcon from '@mui/icons-material/Error';
-import { CreateToastEvent } from "@kvalitetsit/hjemmebehandling/Events/CreateToastEvent";
+import { CreateToastEvent, CreateToastEventData } from "@kvalitetsit/hjemmebehandling/Events/CreateToastEvent";
 interface Props {
     match: { params: { questionnaireId: string } };
     startQuestionIndex?: number;
@@ -86,7 +86,7 @@ export default class QuestionnaireResponseCreationPage extends Component<Props, 
     render(): JSX.Element {
         this.initializeServices();
         if (this.state.submitted) {
-            new CreateToastEvent({ title: "Din besvarelse blev sendt", JsxPrefix: <>Y</>, alertColor: "success", textColor: "white" }).dispatchEvent();
+            new CreateToastEvent(new CreateToastEventData("Din besvarelse blev sendt","","success")).dispatchEvent();
             return (<Redirect push to={"/"} />)
         }
 
