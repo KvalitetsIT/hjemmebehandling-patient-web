d861cc8ce24ba6e219f386aa81efebd36212d6e8
kimfrost <kim@thatworks.dk>
2023-03-29 19:16:23 +0200

    #1B.14.1 - Se mine svar skal være på en linje

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -50,7 +50,6 @@ export class Layout extends Component<{}, State> {
     return (
       <>
         <Box>
-
           <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <ErrorBoundary ekstraText="Fejlen der opstod kræver opdatering af siden (F5)" showReloadButton={true}>
             <Router>
