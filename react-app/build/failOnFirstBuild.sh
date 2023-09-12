47fffc0293456195d8ecff5de4e8d9a5687772b6
jonas <jqh@kvalitetsit.dk>
2023-09-12 11:35:29 +0200

    added a few build files

--- react-app/build/failOnFirstBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "${GITHUB_REPOSITORY}"
-echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/hjemmebehandling-patient-web" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/hjemmebehandling-patient-web" ]; then
-  echo "Please run setup.sh REPOSITORY_NAME"
-  exit 1
-fi
