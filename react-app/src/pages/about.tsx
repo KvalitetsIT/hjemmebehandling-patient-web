f9149d70dcaf013fd928aa471fafed1b1ed59927
Sune Koch Hansen <sune@kvalitetsit.dk>
2024-02-21 08:46:43 +0100

    RIM-1166: opdateret relase tag

--- react-app/src/pages/about.tsx
+++ react-app/src/pages/about.tsx
@@ -33,7 +33,7 @@ export default class AboutPage extends Component<{}> {
                                     </ListItemIcon>
                                     <ListItemText disableTypography
                                         primary={<Typography className="headline" color={'inherit'}>Medicinsk udstyr</Typography>}
-                                        secondary={<Typography>Versionsnummer på software: 2.0.0-2023-06-07</Typography>}
+                                        secondary={<Typography>Versionsnummer på software: 3.0.0-2024-02-22</Typography>}
                                     />
                                 </ListItem>
 
