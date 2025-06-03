ff0d5331e4f5735510ff05489ef12782c395233a
Jonas <jqh@kvalitetsit.dk>
2025-06-03 16:24:36 +0200

    updated version

--- react-app/src/pages/about.tsx
+++ react-app/src/pages/about.tsx
@@ -33,7 +33,7 @@ export default class AboutPage extends Component<{}> {
                                     </ListItemIcon>
                                     <ListItemText disableTypography
                                         primary={<Typography className="headline" color={'inherit'}>Medicinsk udstyr</Typography>}
-                                        secondary={<Typography>Versionsnummer på software: 3.0.1-2024-03-19</Typography>}
+                                        secondary={<Typography>Versionsnummer på software: 4.0.0-2025-06-04</Typography>}
                                     />
                                 </ListItem>
 
